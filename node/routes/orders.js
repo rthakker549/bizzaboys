0551c84858038a1dddfb30ac578d7130a44acbcd
rthakker549 <rthakker549@gmail.com>
2019-10-12 15:51:39 -0500

    Added Basic Order BackEnd

---
+++ node/routes/orders.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+let bc = require('badcube');
+
+/* GET all Orders. */
+router.get('/', function (req, res, next) {
+    let allOrders = bc.Orders.findAll({});
+    res.json(allOrders);
+});
+
+router.post('/order', function(req,res) {
+    let firstName = req.firstName;
+    let lastName = req.lastName;
+    let email = req.email;
+    let pizza = req.pizza;
+    let building = req.string;
+    let room = req.room;
+    
+    Orders.insert({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        pizza: pizza,
+        building: building,
+        room: room
+    })
+
+    req.send("Order Completed")
+})
+
+
+module.exports = router;
---
+++ node/schemas/Orders.js
@@ -0,0 +1,8 @@
+module.exports = {
+    firstName: String,
+    lastName: String,
+    email: String,
+    pizza: String,
+    building: String,
+    room: String
+}
