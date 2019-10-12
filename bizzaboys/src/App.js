76ac47052c3bb7679c522512ad9ff87f8c622d7b
AndreGarivay <andregarivay@gmail.com>
2019-10-12 14:12:53 -0500

    update

--- bizzaboys/src/App.js
+++ bizzaboys/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Navbar, NavbarBrand, Nav, NavLink, NavItem } from "shards-react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css"
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar>
+        <NavbarBrand>Bizza</NavbarBrand>
+        <Nav>
+        <NavItem>
+              <NavLink active href="#">
+                Order Now
+              </NavLink>
+            </NavItem>
+            </Nav>
+      </Navbar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
