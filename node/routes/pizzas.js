50e4fdba7b7dd89b774a12e859c7a1b3ecc52150
Alexis Renderos <renderosalexis17@gmail.com>
2019-10-12 23:50:27 -0500

    Created functions for pizzas. -Alexis

--- node/routes/pizzas.js
+++ node/routes/pizzas.js
@@ -2,12 +2,20 @@ var express = require('express');
 var router = express.Router();
 let bc = require('badcube');
 
-/* GET all Orders. */
+/* GET all pizzas. */
 router.get('/', function (req, res, next) {
     let allPizzas = bc.Pizzas.findAll({});
     res.json(allPizzas);
 });
 
+/* GET all available pizzas. */
+router.get('/', function (req, res, next) {
+    let allPizzas = bc.Pizzas.findAll({});
+    let availablePizzas = allPizzas.filter(x => x.inventory > 0);
+    res.json(availablePizzas);
+});
+
+/* POST new pizza to database. */
 router.post('/pizza', function(req,res) {
     let pizzaName = req.pizzaName;
     let description = req.description;
@@ -26,5 +34,23 @@ router.post('/pizza', function(req,res) {
     res.send("Pizza Completed")
 })
 
+/* PUT pizza inventory. */
+router.put('/decreasePizzaInventory', function(req,res) {
+    let pizzaName = req.query.pizzaName;
+    let pizza = bc.Pizzas.find({pizzaName:pizzaName})
+    let inventory = pizza.inventory;
+    inventory--;
+    bc.Pizzas.update({pizzaName:pizzaName},{inventory:inventory});
+    res.send("Pizza Inventory Update Completed")
+})
+
+
+/* Delete a 'zza */
+router.put('/deletePizza', function(req,res) {
+    let pizzaName = req.query.pizzaName;
+    let pizza = bc.Pizzas.find({pizzaName:pizzaName})
+    bc.Pizzas.delete({pizzaName:pizzaName});
+    res.send("Pizza Inventory Update Completed")
+})
 
 module.exports = router;
